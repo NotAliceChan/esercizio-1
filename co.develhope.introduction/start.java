0711c633861b61734fb7c8c9d988322054edd5d4
Alice <fenuwu99@gmail.com>
2023-06-06 15:08:33 +0200

    esercizio uno

---
+++ co.develhope.introduction/start.java
@@ -0,0 +1,7 @@
+package co.develhope.introduction;
+
+public class start {
+    public static void main(String[] args) {
+        System.out.println("Hello develhope java student! My birthday is 30/01/1999.");
+    }
+}
